da2d350e56eea55909aced5b89a349cdc85a7f02
girish-kadadi <151708025+girish-kadadi@users.noreply.github.com>
2023-11-22 11:09:53 +0530

    Create Day1_M_Names.cs

---
+++ Day1_M_Names.cs
@@ -0,0 +1,16 @@
+review1.cs
+int d; // elapsed time in days
+int elapsedTimeInDays;
+
+review2.cs
+var dataFromDb = db.GetFromService().ToList(); // Get List of employees
+var listOfEmployeeNames = db.employeeService.GetEmployeeName().ToList();
+
+review3.cs
+int iCounter;
+string strFullName;
+DateTime dModifiedDate;
+
+int Counter;
+string FullName;
+DateTime ModifiedDate;
